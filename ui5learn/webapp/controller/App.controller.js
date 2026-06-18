0efb9d5865e97d67b9f21039f4ce68a8517d608e
Aman Soni <sonisir1920@gmail.com>
2026-06-18 07:44:22 +0000

    json model used

--- ui5learn/webapp/controller/App.controller.js
+++ ui5learn/webapp/controller/App.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
-
+    "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+], function (Controller, JSONModel, MessageToast) {
     "use strict";
      
 return Controller.extend("sap.ui.demo.learn.controller.App",{
+    onInit:function(){
+
+        var oData={
+            recipient:{
+                name:"UI5 Application"
+            }
+        };
+        var oModel=new JSONModel(oData);
+        this.getView().setModel(oModel);        
+    },
+
     onPress:function(){
         MessageToast.show("I am a button!");
     }
