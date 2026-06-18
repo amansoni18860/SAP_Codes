4e16d22ddfd41f6fde957c204d47db0f7184573b
Aman Soni <sonisir1920@gmail.com>
2026-06-18 09:09:29 +0000

    change theme and apply shell to the app

--- ui5learn/webapp/Component.js
+++ ui5learn/webapp/Component.js
@@ -1,28 +1,27 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-],function(UIComponent,JSONModel,ResourceModel){
+    "sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
     "use strict";
 
-    return UIComponent.extend("sap.ui.demo.learn.Component",{
-        metadata:{
-            manifest:"json"
-              
+    return UIComponent.extend("sap.ui.demo.learn.Component", {
+
+        metadata: {
+            manifest: "json"
         },
 
-        init:function(){
-            UIComponent.prototype.init.apply(this,arguments);
+        init: function () {
 
-            var oData={
-                recipient:{
-                    name:"UI5 Team"
+            UIComponent.prototype.init.apply(this, arguments);
+
+            var oData = {
+                recipient: {
+                    name: "UI5 Team"
                 }
             };
-            var oModel=new JSONModel(oData);
+
+            var oModel = new JSONModel(oData);
             this.setModel(oModel);
-
-
         }
-})
-})
+    });
+});
