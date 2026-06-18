f1c81be630987b2aa672e35d42f425386347fb14
Aman Soni <sonisir1920@gmail.com>
2026-06-18 09:54:39 +0000

    add xmlview inside a view

--- ui5learn/webapp/controller/App.controller.js
+++ ui5learn/webapp/controller/App.controller.js
@@ -1,15 +1,9 @@
-sap.ui.define(["sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-    "sap/m/MessageToast"
-], function (Controller, JSONModel, ResourceModel, MessageToast) {
+sap.ui.define(["sap/ui/core/mvc/Controller"
+], function (Controller) {
     "use strict";
      
 return Controller.extend("sap.ui.demo.learn.controller.App",{
 
-    onPress:function(){
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        MessageToast.show(oBundle.getText("helloMessage", [this.getView().getModel().getProperty("/recipient/name")]));
-    }
+  
 });
 })  
---
+++ ui5learn/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, ResourceModel, MessageToast) {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.learn.controller.HelloPanel", {
+
+        onPress: function () {
+            var oBundle = this.getView()
+                .getModel("i18n")
+                .getResourceBundle();
+
+            MessageToast.show(
+                oBundle.getText(
+                    "helloMessage",
+                    [this.getView().getModel().getProperty("/recipient/name")]
+                )
+            );
+        }
+    });
+});
